fb6cd2c8477400c68d8aa6727ac327e3619a76b1
Guillermo Rebolledo <guillermoortizitc@outlook.com>
2018-06-19 16:30:25 -0500

    more codefights

--- sortByHeight.js
+++ sortByHeight.js
@@ -11,6 +11,34 @@
 
     For a = [-1, 150, 190, 170, -1, -1, 160, 180], 
     the output should be:
-    
+
     sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190]
 */
+
+function sortByHeight(a) {
+    let treeIndices = [];
+    let resultArray = [];
+    let sortedArray = a.filter(item => {
+        return item !== -1;
+    }).sort((a,b) =>{
+        return b-a;
+    });
+
+    for(let i = 0; i < a.length; i++){
+        if(a[i] === -1){
+            treeIndices.push(i);
+        }
+    }
+
+    for(let i = 0; i < a.length; i++){
+        if(i === treeIndices[0]){
+            resultArray.push(-1);
+            treeIndices.shift();
+        }else{
+            resultArray.push(sortedArray.pop());
+        }
+    }
+    return resultArray;
+}
+
+sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]);
